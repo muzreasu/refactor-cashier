8658fef520ba72dac163381e65cea1dd4789f092
Xiaofeng Su <xiaofeng.su@thoughtworks.com>
2020-07-14 00:29:40 +0800

    extract method

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,5 +1,7 @@
 package cc.xpbootcamp.warmup.cashier;
 
+import java.util.List;
+
 /**
  * OrderReceipt prints the details of order including customer name, address, description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
@@ -29,31 +31,49 @@ public class OrderReceipt {
 //        output.append(order.getCustomerLoyaltyNumber());
 
         // prints lineItems
-        double totalSalesTax = INITIAL_NUMBER;
+
+        List<LineItem> lineItems = order.getLineItems();
         double totalAmount = INITIAL_NUMBER;
+
+        buildLineItemReceipt(output, lineItems);
+        Double totalSalesTax = lineItems.stream().map(LineItem::getTotalAmount).reduce(INITIAL_NUMBER,
+                (subtotal, lineItem2) -> subtotal + lineItem2 * SALE_TAX_RATE);
+        totalAmount = calculateTotalAmount(totalAmount);
+        buildTotalTaxAndAmount(output, totalSalesTax, totalAmount);
+
+        return output.toString();
+    }
+
+
+    private double calculateTotalAmount(double totalAmount) {
         for (LineItem lineItem : order.getLineItems()) {
+            // calculate sales tax @ rate of 10%
+            double salesTax = lineItem.getTotalAmount() * SALE_TAX_RATE;
+
+            // calculate total amount of lineItem = price * quantity + 10 % sales tax
+            totalAmount += lineItem.getTotalAmount() + salesTax;
+        }
+        return totalAmount;
+    }
+
+    private void buildTotalTaxAndAmount(StringBuilder output, double totalSalesTax, double totalAmount) {
+        // prints the state tax
+        output.append("Sales Tax").append('\t').append(totalSalesTax);
+
+        // print total amount
+        output.append("Total Amount").append('\t').append(totalAmount);
+    }
+
+    private void buildLineItemReceipt(StringBuilder output, List<LineItem> lineItems) {
+        lineItems.forEach((lineItem -> {
             output.append(lineItem.getDescription());
             output.append('\t');
             output.append(lineItem.getPrice());
             output.append('\t');
             output.append(lineItem.getQuantity());
             output.append('\t');
-            output.append(lineItem.totalAmount());
+            output.append(lineItem.getTotalAmount());
             output.append('\n');
-
-            // calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * SALE_TAX_RATE;
-            totalSalesTax += salesTax;
-
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            totalAmount += lineItem.totalAmount() + salesTax;
-        }
-
-        // prints the state tax
-        output.append("Sales Tax").append('\t').append(totalSalesTax);
-
-        // print total amount
-        output.append("Total Amount").append('\t').append(totalAmount);
-        return output.toString();
+        }));
     }
 }
